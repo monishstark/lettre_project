b05efc15985c83ba5ba4b1c93ffb7e3362c7fdc1
monishstark <monishkumarms3@gmail.com>
2021-05-08 00:51:19 +0530

    update

---
+++ app/src/main/java/com/example/lettre/splash_screen.java
@@ -0,0 +1,31 @@
+package com.example.lettre;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class splash_screen extends AppCompatActivity {
+
+    private static int SPLASH_TIME_OUT = 4000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        getSupportActionBar().hide();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent= new Intent(getApplicationContext(),login.class);
+                startActivity(intent);
+
+            }
+        },SPLASH_TIME_OUT);
+
+
+    }
+}
